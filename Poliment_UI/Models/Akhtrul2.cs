1a17530f8be0a4f0904cfba28b7b44c6f159f2e4
Mohd Akhtar <akhtarbas123@gmail.com>
2024-09-30 09:10:59 +0530

    added cs file1

---
+++ Poliment_UI/Models/Akhtrul2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Poliment_UI.Models
+{
+    public class Akhtrul2
+    {
+        public string userId { get; set; }
+        public string password { get; set; }
+        public string sendMethod { get; set; }
+        public string audioType { get; set; }
+
+
+    }
+}
