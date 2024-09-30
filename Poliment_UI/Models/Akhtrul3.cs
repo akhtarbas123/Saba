b3635c8a1009583192bcbdd153f4c7ac6797a750
Mohd Akhtar <akhtarbas123@gmail.com>
2024-09-30 09:34:47 +0530

    added cs3 file

---
+++ Poliment_UI/Models/Akhtrul3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Poliment_UI.Models
+{
+    public class Akhtrul3
+    {
+        public string userId { get; set; }
+        public string password { get; set; }
+    }
+}
